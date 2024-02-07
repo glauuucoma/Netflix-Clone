cf03b9a70eb207ba1ceac905734691cb725bb5b2
glauuucoma <kulebadavid@gmail.com>
2024-02-06 23:47:06 -0500

    add google and github Authentication

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -93,6 +93,7 @@ const Auth = () => {
 
             <div className="flex flex-row items-center gap-4 mt-8 justify-center">
               <div
+                onClick={() => signIn("google", { callbackUrl: "/" })}
                 className="
               w-10 
               h-10 
@@ -108,6 +109,7 @@ const Auth = () => {
                 <FcGoogle size={30} />
               </div>
               <div
+                onClick={() => signIn("github", { callbackUrl: "/" })}
                 className="
               w-10 
               h-10 
